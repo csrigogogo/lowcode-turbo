0c42f01a35e42ae2de2eddeb2643efe5787c62bf
crisri-mac <crifsri@icloud.com>
2022-11-09 13:38:40 +0800

    chore(编译 swc , rollup build): 确定工具

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
-
+// import { Button } from "ui";
+import { Button } from "@lowcode-turbo/component"
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
     </div>
-  );
+  )
 }
---
+++ packages/component/dist/index.js
@@ -0,0 +1,12 @@
+import { jsx, Fragment } from 'react/jsx-runtime';
+
+var Button = (()=>{
+    return /*#__PURE__*/ jsx(Fragment, {
+        children: /*#__PURE__*/ jsx("button", {
+            children: "1111"
+        })
+    });
+});
+
+export { Button };
+//# sourceMappingURL=index.js.map
